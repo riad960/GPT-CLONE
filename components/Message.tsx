03796626b16e6d88c5c6f4e82964ba7f943b8d17
riad960 <riad96048@gmail.com>
2023-03-24 00:50:54 +0600

    just-checking

--- components/Message.tsx
+++ components/Message.tsx
@@ -10,7 +10,7 @@ const isGPT = message.user.name==="CHATGPT"
   return (
   <div className={`py-5  overflow-hidden ${isGPT && 'bg-gray-500'}`}>
     <div className="flex  space-x-5 px-10 max-w-2xl mx-5">
-        <img src={message?.user.avatar} alt="" className="h-8 w-8 rounded-full" />
+        <img src={message?.user.avatar} alt="" className="h-8 w-8 rounded-full object-cover" />
         <p className="pt-1 text-sm">{message.text}</p>
     </div>
   </div>
